d87c7418d52c5ffeec2fbe042a921b4aa8359667
m1h4wkx <garkanto@gmail.com>
2025-07-24 21:33:16 -0600

    Update cpa.js

--- resources/js/cpa.js
+++ resources/js/cpa.js
@@ -1,5 +1,5 @@
  var width = 0,
-                url = "💕 Packs Filtrados 😈🔥 http://eshub.live/",
+                url = "💕 Únete a los mejores Packs Prohibi2 😈👉 http://eshub.live/",
  
                 share = "https://t.me/share/url?url=" + url,
                 cpa = "https://href.li/?http://acort.link/mB5zJw";
